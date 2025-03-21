bc2aee075503e90f897e10455e3937da8e3e0976
maxime <trousselmaxime@gmail.com>
2025-03-21 09:02:36 +0100

    config

---
+++ client/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,12 @@
+export default function NavBar() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>Home</li>
+        <li>créer</li>
+        <li>modifier</li>
+        <li>supprimer</li>
+      </ul>
+    </nav>
+  );
+}
--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,21 +1,52 @@
-create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+CREATE TABLE cds (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    band_name VARCHAR(255) NOT NULL,
+    album_name VARCHAR(255) NOT NULL,
+    release_year YEAR NOT NULL,
+    cover VARCHAR(255)NULL
 );
 
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
+CREATE TABLE reviews (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cd_id INT NOT NULL UNIQUE,
+    rating INT CHECK (rating BETWEEN 1 AND 5) NULL,
+    review_text TEXT NULL,
+    FOREIGN KEY (cd_id) REFERENCES cds(id) ON DELETE CASCADE
 );
 
-insert into user(id, email, password)
-values
-  (1, "jdoe@mail.com", "123456");
 
-insert into item(id, title, user_id)
+INSERT INTO cds (band_name, album_name, release_year)
 values
-  (1, "Stuff", 1),
-  (2, "Doodads", 1);
+   ('Muse', 'Showbiz', 1999),
+    ('Muse', 'Origin of Symmetry', 2001),
+    ('Muse', 'Absolution', 2003),
+    ('Muse', 'Black Holes and Revelations', 2006),
+    ('Muse', 'The Resistance', 2009),
+    ('Muse', 'The 2nd Law', 2012),
+    ('Muse', 'Drones', 2015),
+    ('Muse', 'Simulation Theory', 2018),
+    ('Muse', 'Will of the People', 2022);
+
+INSERT INTO reviews (cd_id, rating, review_text)
+values
+  (1, 4, 'Un excellent début, brut et authentique.'),
+    (2, 5, 'Une évolution impressionnante avec des influences classiques.'),
+    (3, 5, 'des morceaux inoubliables comme Hysteria.'),
+    (4, 5, 'Un album révolutionnaire avec des éléments électroniques.'),
+    (5, 4, 'Un concept album puissant.'),
+    (6, 4, NULL),
+    (7, 4, 'Un retour aux racines rock avec un message fort.'),
+    (8, 3, 'Un album divertissant mais inégal.'),
+    (9, 4, 'Un mélange de tous les styles précédents de Muse.');
+
+
+
+SELECT * FROM cds;
+
+SELECT * FROM reviews;
+
+SELECT * FROM cds LEFT JOIN reviews ON cds.id = reviews.cd_id;
+
+SELECT cds.id, cds.band_name, cds.album_name, cds.release_year, reviews.rating
+FROM cds
+LEFT JOIN reviews ON cds.id = reviews.cd_id;
